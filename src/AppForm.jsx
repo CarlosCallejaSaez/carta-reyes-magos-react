c393a6fd4ca45ed7acb0f0f98f30b0383ff205a3
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-12-26 22:31:07 +0100

    img fixed

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -77,7 +77,7 @@ const AppForm = () => {
       {step === 3 && (
         <div className="formStep successStep">
           <p>Carta enviada con éxito a sus majestades de Oriente</p>
-          <img src="./assets/smartphone-7001038_1280.png" alt=" Sent" />
+          <img src="/public/smartphone-7001038_1280.png" alt=" Sent" />
         </div>
       )}
     </div>
